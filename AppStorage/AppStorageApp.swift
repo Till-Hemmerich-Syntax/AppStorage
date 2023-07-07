354b598ee044a88c3515452cbcc52a9315d4d3b2
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-07-07 10:42:51 +0200

    Initial Commit

---
+++ AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Till Hemmerich on 07.07.23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
